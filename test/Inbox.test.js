57d3ee9a429866f931dd381e86f9c7495f8b94b8
Hung Nguyen <hungnb31@gmail.com>
2021-11-17 16:46:43 +0700

    update more test for deployed contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -21,6 +21,22 @@ beforeEach(async () => {
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change the message", async () => {
+    const newMessage = "Message changed!";
+    // update the message
+    const trxHash = await inbox.methods
+      .setMessage(newMessage)
+      .send({ from: accounts[0] });
+    // get message again to verify it was changed
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
   });
 });
